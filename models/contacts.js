30ed7ce934f1f4e2dbaac511f160d7af86e26a51
olga-zak <olgaliam@gmail.com>
2023-03-04 12:57:36 +0200

    listContacts, getContactById, addContact

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,86 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: id } = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contactsList = await fs.readFile(contactsPath, "utf8");
+    const parsedContactsList = JSON.parse(contactsList);
 
-const removeContact = async (contactId) => {}
+    return parsedContactsList;
+  } catch (error) {
+    return console.log("Something went wrong:", error);
+  }
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contactsList = await fs.readFile(contactsPath, "utf8");
+    const parsedContactsList = JSON.parse(contactsList);
 
-const updateContact = async (contactId, body) => {}
+    const contactToGet = parsedContactsList.filter(
+      ({ id }) => id === contactId.toString()
+    );
+
+    // if (contactToGet.length === 0) {
+    //   return console.log(`Contact with id: ${contactId} is missing`);
+    // }
+
+    return contactToGet;
+  } catch (error) {
+    return console.log("Something went wrong:", error);
+  }
+};
+//addContact(body) для сохранения контакта в файле contacts.json
+const addContact = async (body) => {
+  //!!!!!!!!разобраться с body и newContact!!!!!!!!!!
+  //return body;
+  const newContact = body;
+  // return newContact;
+  try {
+    const contactsList = await fs.readFile(contactsPath, "utf8");
+    const parsedContactsList = JSON.parse(contactsList);
+
+    const nameToAdd = parsedContactsList.find(
+      (contact) => contact.name.toLowerCase() === body.name.toLowerCase()
+    );
+    const emailToAdd = parsedContactsList.find(
+      (contact) => contact.email.toLowerCase() === body.email.toLowerCase()
+    );
+    const phoneToAdd = parsedContactsList.find(
+      (contact) => contact.phone === body.phone
+    );
+
+    if (nameToAdd || emailToAdd || phoneToAdd) {
+      //return console.log("The contact with same data already exists");
+      return { message: "The contact with same data already exists" };
+    }
+    if (!nameToAdd && !emailToAdd && !phoneToAdd) {
+      console.log(`New contact ${body.name} succesfully added to the list`);
+    }
+
+    const updatedContactsList = JSON.stringify([
+      ...parsedContactsList,
+      newContact,
+    ]);
+
+    await fs.writeFile(contactsPath, updatedContactsList, "utf8");
+
+    const contactsListAfterAdding = await fs.readFile(contactsPath, "utf8");
+    //console.log(contactsListAfterAdding);
+    return JSON.parse(contactsListAfterAdding);
+    //return console.table(JSON.parse(contactsListAfterAdding));
+  } catch (error) {
+    console.log("Something went wrong:", error);
+    return { message: "Something went wrong" };
+  }
+};
+
+const removeContact = async (contactId) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +88,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,79 @@
-const express = require('express')
+const express = require("express");
+const { v4: id } = require("uuid");
 
-const router = express.Router()
+const router = express.Router();
+console.log(444);
+const {
+  listContacts,
+  getContactById,
+  addContact,
+} = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// @ GET /api/contacts
+// ничего не получает
+// вызывает функцию listContacts для работы с json-файлом contacts.json
+// возвращает массив всех контактов в json-формате со статусом 200
+router.get("/", async (_, res) => {
+  const data = await listContacts();
+  res.status(200).json(data);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// @ GET /api/contacts/:id
+// Не получает body
+// Получает параметр id
+// вызывает функцию getById для работы с json-файлом contacts.json
+// если такой id есть, возвращает объект контакта в json-формате со статусом 200
+// если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
+router.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const data = await getContactById(id);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (data.length !== 1) {
+    return res.status(404).json({ message: "Not found" });
+  }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json(data);
+  next;
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// @ POST /api/contacts
+// Получает body в формате {name, email, phone} (все поля обязательны)
+// Если в body нет каких-то обязательных полей, возвращает json с ключом {"message": "missing required name field"} и статусом 400
+// Если с body все хорошо, добавляет уникальный идентификатор в объект контакта
+// Вызывает функцию addContact(body) для сохранения контакта в файле contacts.json
+// По результату работы функции возвращает объект с добавленным id {id, name, email, phone} и статусом 201
+router.post("/", async (req, res, next) => {
+  //!!!!!!деструктуризировать!!!!!!
+  const data = req.body;
+  //======BODY VALIADATION==========//
+  //Для маршрутов, что принимают данные (POST и PUT), продумайте проверку (валидацию) принимаемых данных. Для валидации принимаемых данных используйте пакет joi
+  //express validator
+  //https://developer.mozilla.org/ru/docs/Learn/Server-side/Express_Nodejs/forms
+  if (data.name === "" || data.email === "" || data.phone === "") {
+    res.status(400).json({ message: "missing required name field" });
+    return;
+  }
+  //======BODY VALIADATION==========//
+  const newContact = {
+    id: id(),
+    name: data.name,
+    email: data.email,
+    phone: data.phone,
+  };
 
-module.exports = router
+  await addContact(newContact);
+
+  res.status(201).json(newContact);
+
+  next;
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "4template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "5template message" });
+});
+
+module.exports = router;
