13d18f1010e21bc427d03c370e2c4f7fd64fc674
olga-zak <olgaliam@gmail.com>
2023-03-11 11:53:08 +0200

    Update contactSchema.js

--- services/contactSchema.js
+++ services/contactSchema.js
@@ -1,28 +1,20 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
 const { Schema, model } = require("mongoose");
 
 const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
-  favorite: Boolean,
-  // name: {
-  //   type: String,
-  //   // required: [true, "Set name for contact"],
-  //   // unique: true,
-  // },
-  // email: {
-  //   type: String,
-  //   // unique: true,
-  // },
-  // phone: {
-  //   type: String,
-  //   // unique: true,
-  // },
-  // favorite: {
-  //   type: Boolean,
-  // },
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Contact = model("contact", contactSchema);
