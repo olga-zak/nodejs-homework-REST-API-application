5e213bbc4f2277ab3ce44643057f1d815b899093
olga-zak <olgaliam@gmail.com>
2023-03-11 12:30:22 +0200

    Update validation.js

--- validation/validation.js
+++ validation/validation.js
@@ -4,6 +4,7 @@ const bodyValidation = Joi.object({
   name: Joi.string().alphanum().min(3).max(30).required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
+  favorite: Joi.bool().default(false),
 });
 
 const updateContactValidation = Joi.object({
