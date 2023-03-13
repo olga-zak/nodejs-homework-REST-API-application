00b920a0fb7ad52dec63fa3c47f0e8cffaf83dfe
olga-zak <olgaliam@gmail.com>
2023-03-13 14:03:49 +0200

    hash password

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const { User } = require("../../schemas/userSchema");
 
 const { returnError } = require("../../helpers");
@@ -10,8 +12,9 @@ const register = async (req, res, next) => {
   if (user) {
     throw returnError(409, `email ${email} already exists`);
   }
-  //if email doesn't exist -> add it to the db
-  const result = await User.create({ name, email, password });
+  //if email doesn't exist -> захешировать password + add user to the db
+  const hashPassword = await bcrypt.hash(password, 10);
+  const result = await User.create({ name, email, password: hashPassword });
   res.status(201).json({
     name: result.name,
     email: result.email,
