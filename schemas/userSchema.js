3271861ba1ce1724865e56436a95bd56f6aac3b7
olga-zak <olgaliam@gmail.com>
2023-03-15 01:17:23 +0200

    update userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -7,22 +7,42 @@ const emailRegexp = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
 
 //===========mongoose Schema===========//
 const userSchema = new Schema(
+  // {
+  //   name: {
+  //     type: String,
+  //     //required: true,
+  //     required: [true, "Contact name should exist"],
+  //   },
+  //   email: {
+  //     type: String,
+  //     //match: emailRegexp,
+  //     //unique: true, //4.1 lesson 22:55 //3.2 lesson 1:24
+  //     required: true,
+  //   },
+  //   password: {
+  //     type: String,
+  //     // minlength: 6,
+  //     required: true,
+  //   },
+  // },
   {
-    name: {
+    password: {
       type: String,
-      //required: true,
-      required: [true, "Contact name should exist"],
+      required: [true, "Password is required"],
     },
     email: {
       type: String,
-      //match: emailRegexp,
-      //unique: true, //4.1 lesson 22:55 //3.2 lesson 1:24
-      required: true,
+      required: [true, "Email is required"],
+      //unique: true,
     },
-    password: {
+    subscription: {
       type: String,
-      // minlength: 6,
-      required: true,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
     },
   },
   { versionKey: false, timestamps: true }
@@ -35,10 +55,11 @@ userSchema.post("save", handleSchemaValidationErrors);
 
 //===========joi Schema===========//
 const registerUserJoiSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  //email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().required(),
+  email: Joi.string().required(),
+  //email: Joi.string().pattern(emailRegexp).required(), //+unique
+  subscription: Joi.string().default("starter"),
+  token: Joi.string().default(null),
 });
 
 const loginUserJoiSchema = Joi.object({
