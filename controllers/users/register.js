b9615935a93fd9b598fbc6fa85663f0c119b13fb
olga-zak <olgaliam@gmail.com>
2023-03-15 03:00:29 +0200

    clear code

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -6,13 +6,12 @@ const { returnError } = require("../../helpers");
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
-  console.log(email);
-  //check if email exists
+  //check if email exists in db
   const user = await User.findOne({ email });
   if (user) {
-    throw returnError(409, `email ${email} already exists`);
+    throw returnError(409, `email ${email} is in use`);
   }
-  //if email doesn't exist -> захешировать password + add user to the db
+  //if email doesn't exist in db -> захешировать password + add user to the db
   const hashPassword = await bcrypt.hash(password, 10);
   const result = await User.create({ email, password: hashPassword });
   res.status(201).json({
