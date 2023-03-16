f1fed28c99a093d100181b42c4288fe8f3128c03
olga-zak <olgaliam@gmail.com>
2023-03-16 19:01:13 +0200

    Update addContactController.js

--- controllers/addContactController.js
+++ controllers/addContactController.js
@@ -1,7 +1,7 @@
 const { addContact } = require("../services/contactsService");
 
 const addContactController = async (req, res, next) => {
-  console.log(req.user);
+  // console.log(req.user);
   const { _id: owner } = req.user;
   const newContact = req.body;
 
