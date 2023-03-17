ea27b7e7217623eee16b37c63bd2d614b7d96f16
olga-zak <olgaliam@gmail.com>
2023-03-17 15:09:12 +0200

    Update updateUserAvatar.js

--- controllers/users/updateUserAvatar.js
+++ controllers/users/updateUserAvatar.js
@@ -3,7 +3,6 @@ const path = require("path");
 const Jimp = require("jimp");
 
 const { User } = require("../../schemas/userSchema");
-const { trusted } = require("mongoose");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
