ba2212d338bca006145d61cfd84ecd986a32cdae
olga-zak <olgaliam@gmail.com>
2023-03-26 20:55:14 +0300

    Create verifyEmail.js

---
+++ controllers/users/verifyEmail.js
@@ -0,0 +1,22 @@
+//const nodemailer = require("nodemailer");
+
+const { User } = require("../../schemas/userSchema");
+
+const { returnError } = require("../../helpers");
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw returnError(404, "User not found");
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: "",
+  });
+  res.json({
+    message: "Verification successful",
+  });
+};
+
+module.exports = verifyEmail;
